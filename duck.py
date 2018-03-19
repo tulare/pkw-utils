572814cdc903d29306edac1137268e1bf265cbed
tulare <tulare.paxgalactica@gmail.com>
2018-03-19 19:20:02 +0100

    Rewrite of utils.duck module

--- duck.py
+++ duck.py
@@ -1,58 +1,82 @@
 # -*- encoding: utf-8 -*-
 
-import requests
 from lxml.etree import HTML
-from urllib.parse import urlparse, parse_qs, unquote
+from urllib.parse import urlparse, parse_qs, unquote, urljoin
+from html import unescape
 import uuid
 
-from .config import Configuration
+class FetchError(Exception) :
+    pass
 
-class DuckImageResult :
+class FetchDone(Exception) :
+    pass
 
-    def __init__(self, result) :
-        self.data = dict()
-        for key in ('title', 'width', 'height') :
-            self.data[key] = result[key]
-        self.data['url'] = unquote(result['url'])
-        self.data['image'] = unquote(result['image'])
-        self.data['thumbnail'] = unquote(result['thumbnail'])
+DUCK_IMAGE = {
+    'image'     : ('image',     True,  (unquote,)),
+    'title'     : ('title',     False, ()),
+    'url'       : ('url',       False, (unquote,)),
+    'width'     : ('width',     False, ()),
+    'height'    : ('height',    False, ()),    
+    'thumbnail' : ('thumbnail', False, (unquote,)),
+}
+
+DUCK_TEXT = {
+    'url'       : ('u', True,  (unquote,)),    
+    'site'      : ('i', False, ()),
+    'title'     : ('t', False, (unescape,)),
+    'text'      : ('a', False, (unescape,)),
+    'provider'  : ('s', False, ()),
+}
+
+DUCK_FILTER = {
+    'STRICT' : None,
+    'MODERATE' : -1,
+    'DISABLE' : -2
+}
+
+class DuckStore :
+
+    def __init__(self, source, descriptor) :
+        self.source = source
+        self.data = {}
+        for key, props in descriptor.items() :
+            name, is_uuid, post_traits = props
+            if is_uuid :
+                self.key_uuid = key
+            try :
+                self.data[key] = self.source[name]
+                for p in post_traits :
+                    self.data[key] = p(self.data[key])
+            except :
+                self.data[key] = None
+
+    def __getattr__(self, attr) :
+        try :
+            return self.data[attr]
+        except KeyError :
+            raise AttributeError
 
     @property
     def uuid(self) :
-        return uuid.uuid3(uuid.NAMESPACE_URL, self.data['image'])
+        return uuid.uuid3(uuid.NAMESPACE_URL, self.data[self.key_uuid])
 
-    @property
-    def url(self) :
-        return self.data['url']
-    
-    @property
-    def image(self) :
-        return self.data['image']
+class DuckSearch :
 
-    @property
-    def thumbnail(self) :
-        return self.data['thumbnail']
+    def __init__(self, session, lang=None, pFilter=None) :
+        self.session = session
 
-    @property
-    def title(self) :
-        return self.data['title']
-    
-    @property
-    def width(self) :
-        return self.data['width']
+        self.lang = lang or 'wt-wt'
+        self.filter = str(pFilter) if pFilter else None
+        self.cookies = {
+            'p' : self.filter,
+            'ah' : 'wt-wt',
+            'l' : self.lang,
+        }
 
-    @property
-    def height(self) :
-        return self.data['height']
+        self.descriptor = DUCK_TEXT
+        self.script = 'd.js'
+        self.script_next = None
 
-class DuckImageSearch :
-
-    def __init__(self) :
-        self.config = Configuration()
-        self.session = requests.Session()
-        self.session.headers.update({
-            'User-Agent' : self.config.get('USER_AGENT')
-        })
         self.reset()
 
     @property
@@ -70,16 +94,13 @@ class DuckImageSearch :
     def search(self, search) :
         self.params = dict()
         
-        params = dict()
-        params['q'] = search
-        params['t'] = 'ffsb'
-        params['ia'] = 'images'
-        params['iar'] = 'images'
-        params['iax'] = 'images'
-
         page = self.session.get(
             'https://duckduckgo.com/',
-            params=params
+            params={
+                'q' : search,
+                't' : 'ffsb',
+            },
+            cookies=self.cookies
         )
         tree = HTML(page.content)
         script = tree.xpath('//script[contains(.,".js")]')[0]
@@ -92,32 +113,110 @@ class DuckImageSearch :
         ).split("'")[1]
 
         script_params = parse_qs(urlparse(script_url).query)
+        # print(script_params)
+
+        for key in script_params :
+            self.params[key] = script_params[key][0]
 
         self.params['o'] = 'json'
-        self.params['p'] = -1
-        for key in ('l', 'q', 'vqd') :
-            self.params[key] = script_params[key]
 
         self.prepared = True
 
-        
+
     def fetch(self, start=0) :
 
         if not self.prepared :
-            return
-        
+            raise FetchError('search not prepared')
+
+        self.script_next = None
         self.params['s'] = start
+        self.params['p'] = -1 if self.filter else 1
 
-        jsi = self.session.get(
-            'https://duckduckgo.com/i.js',
-            params=self.params
-        )
+        try :
+            jsi = self.session.get(
+                urljoin(
+                    'https://duckduckgo.com',
+                    self.script
+                ),
+                params=self.params,
+                cookies=self.cookies
+            )
 
-        for result in jsi.json()['results'] :
-            r = DuckImageResult(result)
-            self._results[r.uuid] = r
+            json = jsi.json()
 
-    def mfetch(self, start=0, num=100) :
-        for lot in range(start, start + num, 50) :
-            self.fetch(lot)
+            for result in json['results'] :
+                if 'n' in result :
+                    self.script_next = result['n']
+                else :
+                    r = DuckStore(result, self.descriptor)
+                    self._results[r.uuid] = r
+
+            if 'next' in json :
+                self.script_next = json['next']
+            
+        except Exception as e :
+            raise FetchError('{!r}'.format(e))
+
+        return self.length
+
+    def fetch_next(self) :
+
+        if not self.prepared :
+            raise FetchError('search not prepared')
+
+        if self.script_next is None :
+            raise FetchDone('no data to fetch')
+
+        script_next = self.script_next
+
+        try :
+            self.script_next = None
+            jsi = self.session.get(
+                urljoin(
+                    'https://duckduckgo.com',
+                    script_next
+                ),
+                params={ 'vqd' : self.params['vqd'] },
+                cookies=self.cookies                    
+            )
+
+            json = jsi.json()
+
+            for result in json['results'] :
+                if 'n' in result :
+                    self.script_next = result['n']
+                else :
+                    r = DuckStore(result, self.descriptor)
+                    self._results[r.uuid] = r
+
+            if 'next' in json :
+                self.script_next = json['next']
+
+        except Exception as e :
+            raise FetchDone('{!r}'.format(e))
+            
+
+        return self.length
+
+    def fetch_all(self) :
+        if not self.prepared :
+            raise FetchError('search not prepared')
+
+        self.fetch()
+        while True :
+            try :
+                self.fetch_next()
+            except FetchDone :
+                break
+
+        return self.length
+    
+
+class DuckImageSearch(DuckSearch) :
+
+    def __init__(self, session, lang=None, pFilter=None) :
+        super().__init__(session, lang, pFilter)
+        self.script = 'i.js'
+        self.descriptor = DUCK_IMAGE
+
         
--- locator.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-import os
-
-ROOT_PATH = os.path.dirname(
-    os.path.dirname(__file__)
-)
-
-DATA_PATH = ROOT_PATH + '\\data\\'
